5d067995e84eee6a81c62f81a6af7b6129a65c29
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-21 22:18:36 +0530

    Easy

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        if (nums.size() == 0) return 0;
+        int sum = 0, ans = INT_MIN;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            ans = max(ans, sum);
+            if (sum < 0) sum = 0;   
+        }
+        
+        return ans;
+    }
+};
