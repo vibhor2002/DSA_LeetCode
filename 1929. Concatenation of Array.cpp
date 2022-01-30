0ac167e3a8f98bca9795e9b1ef134aed2d44500c
Vibhor Grover <grovervibhor21@gmail.com>
2022-01-30 22:39:55 +0530

    Easy

---
+++ 1929. Concatenation of Array.cpp
@@ -0,0 +1,32 @@
+// 1st Method using extra space.
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums)
+    {
+        vector<int>ans;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+
+// 2nd method without extra Space.
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
