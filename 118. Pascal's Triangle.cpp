cb22fe04296d125b3ff816cb7d418ff7e7524f61
Vibhor Grover <grovervibhor21@gmail.com>
2022-02-01 20:32:20 +0530

    Easy

--- 118. Pascal's Triangle.cpp
+++ 118. Pascal's Triangle.cpp
@@ -1,14 +1,19 @@
-vector<vector<int> > generate(int numRows)
+class Solution
 {
-    vector<vector<int> > ret;
-    for (int i = 0; i < numRows; i++)
+public:
+    vector<vector<int> > generate(int numRows)
     {
-        vector<int> row(i + 1, 1);
-        for (int j = 1; j < i; j++)
+        vector<vector<int> > m(numRows); // creating 2-D vector with rows=numRows
+
+        for (int i = 0; i < numRows; i++)
         {
-            row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
+            m[i].resize(i + 1);    // to increase column size as level increases
+            m[i][0] = m[i][i] = 1; // first and last element of every row is 1 always so
+
+            // loop to calculate sum of remaining elements from previous row
+            for (int j = 1; j < i; j++)
+                m[i][j] = m[i - 1][j - 1] + m[i - 1][j];
         }
-        ret.push_back(row);
+        return m;
     }
-    return ret;
-}
+};
