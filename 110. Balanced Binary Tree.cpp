8c23a55dd9a7c9d6be5adcebb76f3f05e10e3f4d
Vibhor Grover <grovervibhor21@gmail.com>
2022-02-04 23:05:56 +0530

    Easy

---
+++ 110. Balanced Binary Tree.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    int balance(TreeNode *node)
+    {
+
+        if (!node)
+        {
+            return 0;
+        }
+        int lh = balance(node->left);
+        int rh = balance(node->right);
+
+        if (lh == -1 || rh == -1) // if either of them is -1, we dont have to go further and just return -1
+            return -1;
+
+        if (abs(lh - rh) > 1) // abs() gives us the absolute difference of the two heights
+            return -1;
+
+        return max(lh, rh) + 1;
+    }
+public:
+    bool isBalanced(TreeNode *root)
+    {
+
+        return (balance(root) != -1); // if the tree is balanced it`ll be positive(true) or else it`ll be definitely negative(false).
+    }
+};
