aef45ad399d1862996c769e07be3085b73439af8
Vibhor Grover <grovervibhor21@gmail.com>
2022-02-06 23:01:44 +0530

    Easy

--- 283. Move Zeroes.cpp
+++ 283. Move Zeroes.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
-   void moveZeroes(vector<int>& nums)
-{
-    stable_partition(begin(nums),end(nums),[](int i){return i;});
-}
+    void moveZeroes(vector<int>& nums) {
+        int size = nums.size();
+        
+        int a=0;
+        
+        for(int i=0; i<size; i++) {
+            if(nums[i]!=0) {
+                swap(nums[i], nums[a]);
+                a++;
+            }
+        }
+    }
 };
