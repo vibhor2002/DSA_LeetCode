767385e6205464b48ab2390297da2eb8305043b1
Vibhor Grover <grovervibhor21@gmail.com>
2022-02-09 23:15:27 +0530

    Easy Optimized Approach

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,10 +1,24 @@
-class Solution
-{
+class Solution {
 public:
-    bool isAnagram(string s, string t)
-    {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+    bool isAnagram(string s, string t) {
+        
+        // Declaring hashmap via vector
+        vector<int> hash(26, 0);
+        
+        for(int i=0; s[i]!='\0'; i++) {
+            hash[s[i]-'a']+=1;
+        }
+        
+        for(int i=0; t[i]!='\0'; i++) {
+            hash[t[i]-'a']-=1;
+        }
+        
+        for(int i=0; i<hash.size(); i++) {
+            if(hash[i]>0 or hash[i]<0) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
