d136c3a9dc9cdaee57229172323506cbffeea06d
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-09 17:51:32 +0530

    Easy Array, Prefix sum

--- 1480. Running Sum of 1d Array.cpp
+++ 1480. Running Sum of 1d Array.cpp
@@ -1,14 +1,36 @@
-// Approach 2: Using Input Array for Output
+// Approach 1: Using Input Array for Output
 // Time Complexity O(n)
 // Space Complexity O(1)
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        int n= nums.size();
-        for(int i=1;i<n;i++){
-            nums[i]+=nums[i-1];
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 1; i < n; i++)
+        {
+            nums[i] += nums[i - 1];
         }
         return nums;
+    }
+};
+
+// Approach 2: using seperate space
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans = {nums[0]};
+        for (int i = 1; i < n; i++)
+        {
+            ans.push_back(ans.back() + nums[i]);
+        }
+        return ans;
     }
 };
