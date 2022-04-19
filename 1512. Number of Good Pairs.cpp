faa3b35faea3f938ab35cf93356d2835020dbf07
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-19 21:06:55 +0530

    Easy Array, Hash Table, Math, Counting

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,21 @@
+// Time complexity: O(n^2)
+
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int c = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+};
