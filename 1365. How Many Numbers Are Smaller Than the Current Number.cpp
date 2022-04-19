dd9e60715a651a408d3aa01ae37f315fe8f7fe7d
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-19 23:12:39 +0530

    Easy Array, Hash Table, Sorting, Counting

--- 1365. How Many Numbers Are Smaller Than the Current Number.cpp
+++ 1365. How Many Numbers Are Smaller Than the Current Number.cpp
@@ -21,3 +21,36 @@ public:
         return ans;
     }
 };
+
+// Time Efficient Solution
+
+class Solution
+{
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+    {
+        // create bucket for counting sort
+        vector<int> a(102);
+        vector<int> b(102);
+
+        // get frequency for each element
+        for (int i = 0; i < nums.size(); i++)
+        {
+            a[nums[i]]++;
+        }
+
+        // filling the prefix sum srray
+        for (int i = 1; i < 102; i++)
+        {
+            b[i] = b[i - 1] + a[i - 1];
+        }
+
+        // populate the result
+        vector<int> result(nums.size());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            result[i] = b[nums[i]];
+        }
+        return result;
+    }
+};
