ee8dea324876741840b8251eeff23624a12f578d
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-26 23:50:57 +0530

    Medium Linked List, Math, Recursion

--- 2. Add Two Numbers.cpp
+++ 2. Add Two Numbers.cpp
@@ -8,60 +8,73 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    void Insert(struct ListNode** head, int newdata){
+    void Insert(struct ListNode **head, int newdata)
+    {
         struct ListNode *newNode = new ListNode(newdata);
-        if(*head == NULL)
+        if (*head == NULL)
             *head = newNode;
-        else{
+        else
+        {
             struct ListNode *temp = *head;
-            while(temp->next != NULL)
+            while (temp->next != NULL)
                 temp = temp->next;
             temp->next = newNode;
         }
     }
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
         struct ListNode *head = NULL;
-        int temp = 0,num;
-        while(l1 != NULL && l2 != NULL){
+        int temp = 0, num;
+        while (l1 != NULL && l2 != NULL)
+        {
             num = l1->val + l2->val + temp;
-            if(num > 9){
+            if (num > 9)
+            {
                 temp = num / 10;
                 num = num % 10;
             }
-            else{
+            else
+            {
                 temp = 0;
             }
             Insert(&head, num);
             l1 = l1->next;
             l2 = l2->next;
         }
-        while(l1 != NULL){
+        while (l1 != NULL)
+        {
             num = l1->val + temp;
-            if(num > 9){
+            if (num > 9)
+            {
                 temp = num / 10;
                 num = num % 10;
             }
-            else{
+            else
+            {
                 temp = 0;
             }
             Insert(&head, num);
             l1 = l1->next;
         }
-        while(l2 != NULL){
+        while (l2 != NULL)
+        {
             num = l2->val + temp;
-            if(num > 9){
+            if (num > 9)
+            {
                 temp = num / 10;
                 num = num % 10;
             }
-            else{
+            else
+            {
                 temp = 0;
             }
             Insert(&head, num);
             l2 = l2->next;
         }
-        if(temp != 0)
+        if (temp != 0)
             Insert(&head, temp);
         return head;
     }
