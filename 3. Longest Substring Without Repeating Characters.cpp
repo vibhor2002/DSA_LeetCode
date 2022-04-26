afeea18bcacdee07c0ea682d70b338f91bebf8ef
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-26 23:51:32 +0530

    Medium Hash Table, String, Sliding window

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -1,22 +1,27 @@
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string str) {
+    int lengthOfLongestSubstring(string str)
+    {
         int len = str.length();
         int max = 0, curlen = 0;
         vector<char> v1;
-        vector<char> :: iterator it,it1;
-        
-        for(int i=0;i<len;i++){
+        vector<char>::iterator it, it1;
+
+        for (int i = 0; i < len; i++)
+        {
             it = find(v1.begin(), v1.end(), str[i]);
-            if(it != v1.end()){
+            if (it != v1.end())
+            {
                 int count = 1;
                 it1 = v1.begin();
-                
-                while(it1 != it){
+
+                while (it1 != it)
+                {
                     it1++;
                     count++;
                 }
-                if(it == v1.begin())
+                if (it == v1.begin())
                     v1.erase(it);
                 else
                     v1.erase(v1.begin(), ++it);
@@ -24,11 +29,12 @@ public:
                 curlen++;
                 curlen = curlen - count;
             }
-            else{
+            else
+            {
                 v1.push_back(str[i]);
                 curlen++;
             }
-            if(curlen > max)
+            if (curlen > max)
                 max = curlen;
         }
         return max;
